fd12c16c1c6bc546ec1f6520a18db6c84064dee8
Ryan Nowakowski <tubaman@fattuba.com>
2016-03-06 22:06:24 -0600

    initial

---
+++ README.md
@@ -0,0 +1,33 @@
+# google-login
+
+Here's a simple python script that logs in to your Google account and
+saves the cookies that you can then use with curl, python-requests or
+whatever.
+
+
+## Dependencies
+
+We use buildout to handle all the dependencies.  Just run:
+
+```shell
+python bootstrap.py
+./bin/buildout
+export PATH=$PWD/bin:$PATH
+```
+
+
+## Example
+
+Put your Google credentials in your `$HOME/.netrc` file:
+
+    machine google.com login example@gmail.com password examplepassword
+
+Then run the following command on the command line:
+
+    ./bin/python googlelogin.py cookies.txt
+
+### Why didn't you create a normal python package on pypi?
+
+I use selenium and phantomjs for the Google login which requires nodejs so a
+pure python solution wouldn't work.  Instead of a standard python egg
+installable via pip, I use buildout.
---
+++ googlelogin.py
@@ -0,0 +1,128 @@
+import sys
+import re
+import netrc
+import logging
+import cookielib
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+__all__ = ['Session', 'get_session']
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARN)
+
+
+def setup_webdriver():
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1120, 550)  # https://realpython.com/blog/python/headless-selenium-testing-with-python-and-phantomjs/
+    return driver
+
+
+def copy_cookies_to_session(driver, session):
+    """Copy cookies from selenium webdriver to requests.Session"""
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        session.cookies.set(
+            cookie['name'],
+            cookie['value'],
+            domain=cookie['domain'],
+            path=cookie['path']
+        )
+
+
+class Session(requests.Session):
+    """A Google session"""
+
+    def find_sign_in(self, driver):
+        conditions = [
+            EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Sign in")),
+            EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "SIGN IN")),
+        ]
+        for condition in conditions:
+            try:
+                sign_in = WebDriverWait(driver, 5).until(condition)
+            except:
+                pass
+            else:
+                break
+        return sign_in
+
+    def login(self, username, password):
+        driver = setup_webdriver()
+        try:
+            url = "https://accounts.google.com/login"
+            driver.get(url)
+            driver.find_element_by_id("Email").clear()
+            driver.find_element_by_id("Email").send_keys(username)
+            try:
+                driver.find_element_by_id("Passwd").clear()
+                driver.find_element_by_id("Passwd").send_keys(password)
+            except:
+                driver.find_element_by_id("next").click()
+                condition = EC.visibility_of_element_located((By.ID, "Passwd"))
+                passwd = WebDriverWait(driver, 5).until(condition)
+                passwd.clear()
+                passwd.send_keys(password)
+            driver.find_element_by_id("signIn").click()
+            copy_cookies_to_session(driver, self)
+        except:
+            driver.save_screenshot('/tmp/googlelogin_problem.png')
+            raise
+        finally:
+            driver.quit()
+
+    def is_logged_in(self):
+        url = "https://myaccount.google.com/"
+        response = self.get(url)
+        return not "Sign in" in response.text
+
+    def save(self, filename):
+        cj = cookielib.MozillaCookieJar(filename)
+        for cookie in self.cookies:
+            cj.set_cookie(cookie)
+        cj.save(ignore_discard=True, ignore_expires=True)
+
+    def load(self, filename):
+        cj = cookielib.MozillaCookieJar(filename)
+        cj.load(ignore_discard=True, ignore_expires=True)
+        for cookie in cj:
+            self.cookies.set_cookie(cookie)
+
+
+def get_session(cookies_path="cookies.txt"):
+    username, _, password = netrc.netrc().authenticators("google.com")
+    session = Session()
+    logger.debug("trying to load saved session")
+    try:
+        session.load(cookies_path)
+    except IOError:
+        logger.debug("error loading saved session")
+        logger.debug("logging in")
+        session.login(username, password)
+        session.save(cookies_path)
+    else:
+        logger.debug("loaded saved session")
+        logger.debug("are we still logged in?")
+        if not session.is_logged_in():
+            logger.debug("no so logging in")
+            session.login(username, password)
+            logger.debug("saving session")
+            session.save(cookies_path)
+        else:
+            logger.debug("yup, we're still logged in")
+    return session
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    cookies_path = argv[1]
+    session = get_session(cookies_path)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
