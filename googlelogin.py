0f1d42f7c5e296ba285b51d76c05349b734ed2d4
Ryan Nowakowski <tubaman@fattuba.com>
2016-07-07 13:36:28 -0500

    switch to cookiestxt

--- googlelogin.py
+++ googlelogin.py
@@ -9,8 +9,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from cookiestxt import MozillaCookieJar
 
-from PatchedMozillaCookieJar import MozillaCookieJar
 
 __all__ = ['Session', 'get_session']
 
